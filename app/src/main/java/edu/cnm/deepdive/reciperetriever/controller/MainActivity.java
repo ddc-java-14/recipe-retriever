39df4dbca7f55533e04007d0f65861cf197f3611
nmitchell313 <nmitchell11@cnm.edu>
2021-11-08 22:15:06 -0700

    attempted to fix layout/navigation.

--- app/src/main/java/edu/cnm/deepdive/reciperetriever/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/reciperetriever/controller/MainActivity.java
@@ -1,14 +1,11 @@
 package edu.cnm.deepdive.reciperetriever.controller;
 
 import android.os.Bundle;
-import android.view.Menu;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import com.google.android.material.navigation.NavigationView;
 import edu.cnm.deepdive.reciperetriever.R;
 import edu.cnm.deepdive.reciperetriever.databinding.ActivityMainBinding;
 
@@ -24,30 +21,18 @@ public class MainActivity extends AppCompatActivity {
     binding = ActivityMainBinding.inflate(getLayoutInflater());
     setContentView(binding.getRoot());
 
-    setSupportActionBar(binding.appBarMain.toolbar);
-    DrawerLayout drawer = binding.drawerLayout;
-    NavigationView navigationView = binding.navView;
-    // Passing each menu ID as a set of Ids because each
-    // menu should be considered as top level destinations.
-    appBarConfiguration = new AppBarConfiguration.Builder(
-        R.id.nav_favorites, R.id.nav_cuisines)
-        .setDrawerLayout(drawer)
-        .build();
-    NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-    NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-    NavigationUI.setupWithNavController(navigationView, navController);
-  }
+    setSupportActionBar(binding.toolbar);
 
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.main, menu);
-    return true;
+    NavController navController = Navigation.findNavController(this,
+        R.id.nav_host_fragment_content_main);
+    appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+    NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
   }
 
   @Override
   public boolean onSupportNavigateUp() {
-    NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+    NavController navController = Navigation.findNavController(this,
+        R.id.nav_host_fragment_content_main);
     return NavigationUI.navigateUp(navController, appBarConfiguration)
         || super.onSupportNavigateUp();
   }
